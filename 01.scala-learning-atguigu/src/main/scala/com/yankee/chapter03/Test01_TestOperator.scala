7283fb21cd94f6a7df64b69a7ee49ef51efa7416
Yankee <yangqi199808@gmail.com>
2022-03-22 16:09:05 +0800

    ✨: scala语法学习
    scala语法学习_运算符

--- 01.scala-learning-atguigu/src/main/scala/com/yankee/chapter03/Test01_TestOperator.scala
+++ 01.scala-learning-atguigu/src/main/scala/com/yankee/chapter03/Test01_TestOperator.scala
@@ -16,6 +16,7 @@ object Test01_TestOperator {
 
     val result3 = 10.3 % 3
     println(result3)
+    println("======================")
 
     // 2.关系运算符
     val s1: String = "hello"
@@ -23,7 +24,68 @@ object Test01_TestOperator {
     println(s1 == s2)
     println(s1.equals(s2))
     println(s1.eq(s2))
+    println("======================")
 
     // 3.逻辑运算符
+    def m(n: Int): Int = {
+      println("m被调用")
+      n
+    }
+
+    val n = 1
+    println((4 > 5) && m(n) > 0)
+
+    // 判断一个字符串是否为空
+    def isNotEmpty(str: String): Boolean = {
+      str != null && !"".equals(str.trim)
+    }
+
+    println(isNotEmpty(null))
+    println("======================")
+
+    // 4.赋值运算符
+    var b: Byte = 10
+    // 会报错，类型转换为Int
+    // b += 1
+    // println(b)
+    var i: Int = 12
+    i += 1
+    println(i)
+    println("======================")
+
+    // 5.位运算符
+    // 00111100
+    val a: Byte = 60
+    // 00001101
+    val a1: Byte = 13
+    // 00001100
+    println(a & a1)
+    // 00111101
+    println(a | a1)
+    // 00110001
+    println(a ^ a1)
+    // 11000011
+    println(~a)
+    // a * 2^3
+    println(a << 3)
+    // a / 2^2
+    println(a >> 2)
+
+    val s: Short = -13
+    // 左移后右边补0
+    println(s << 2)
+    // 右移后高位根据符号为补齐，负数补1，正数补0
+    println(s >> 2)
+    println(s >>> 2)
+    println("======================")
+
+    // 6.Scala中运算符的本质
+    val n1: Int = 12
+    val n2: Int = 37
+    println(n1 + n2)
+    println(n1.+(n2))
+
+    println(1.34.*(25))
+    println((7.5 toInt) toString)
   }
 }
